639363fe2378433339a964c313941050f99f6212
Konstantinos Barbopoulos <159767499+KBarbop@users.noreply.github.com>
2024-06-16 19:25:47 +0300

    layout and fetching

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import logImg from "../assets/logo.jpg";
+import Button from "./UI/Button";
+
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logImg} alt="A restaurant" />
+        <h1>ReactFood</h1>
+      </div>
+      <nav>
+        <Button textOnly>Cart (0)</Button>
+      </nav>
+    </header>
+  );
+}
